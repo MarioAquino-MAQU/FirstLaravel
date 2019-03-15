d273e9afeee2b3a5946b3e5aa2fa27b5ac5515d0
Mario Aquino <MAQU@easi.be>
2019-03-15 08:39:54 +0100

    working restservice

---
+++ app/Http/Controllers/PlayerApiController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Player;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Validator;
+
+class PlayerApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([Player::all()], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return response()->json([Player::create($request->all())], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Player $player)
+    {
+        return response()->json([$player], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Player $player)
+    {
+        $player->update($request->all());
+
+        return response()->json([$player], 202);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Player $player)
+    {
+        return response()->json([], 204);
+    }
+}
